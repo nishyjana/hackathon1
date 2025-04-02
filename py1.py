d8cbb40b5167e956ac91ca7281248bd76934ed8c
Nishanthan Janarthanarajah <nishanthanj@verdentra.com>
2025-04-02 20:48:16 +0530

    init

--- py1.py
+++ py1.py
@@ -141,15 +141,15 @@ if token_result and "access_token" in token_result:
 
     user_input = st.chat_input("Enter your message:")
     if user_input:
-        
+        st.session_state['chat_history'].append({"role": "user", "content": user_input})
+        st.rerun()
         msgs = [
             {
                 "role": "user",
                 "content": user_input
             }
         ] 
-        st.session_state['chat_history'].append({"role": "user", "content": user_input})
-        st.rerun()
+        
         resp = oai_client.chat.completions.create(
             model="gpt-4o",
             messages=msgs,
