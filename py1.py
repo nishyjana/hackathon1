3b3db86f0ed8a2aed658110ed062da1521b66bb7
Nishanthan Janarthanarajah <nishanthanj@verdentra.com>
2025-04-05 03:46:37 +0530

    init

--- py1.py
+++ py1.py
@@ -227,10 +227,10 @@ if token_result and "access_token" in token_result:
             }
 
             .user-avatar {
-               background-image: url('https://img.freepik.com/premium-photo/avatar-chatbot-friendly_960911-23127.jpg');
+               background-image: url('https://th.bing.com/th/id/OIP.tCV3UtrtgxkafOz307c-ogAAAA?w=145&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7');
             }
             .assistant-avatar{
-                background-image: url('https://www.verdentra.com/wp-content/uploads/2023/04/verdentra_logo_white.svg');
+                background-image: url('https://img.freepik.com/premium-photo/avatar-chatbot-friendly_960911-23127.jpg');
             }
 
             @keyframes fadeIn {
